df59658bc3aa9421d12a840c6278501dad7f6774
MinhPC2k3 <pcminhbg@gmail.com>
2024-02-23 14:41:29 +0700

    upgrade pubspec

--- lib/mini_app.dart
+++ lib/mini_app.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'bloc_observe.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart';
 
 void initApp() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,3 +12,10 @@ void initApp() {
   runApp(const WeatherApp());
 }
 
+void closeAppUsingSystemPop() {
+  SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+}
+
+void closeAppUsingExit() {
+  exit(0);
+}
